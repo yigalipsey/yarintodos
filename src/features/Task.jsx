c571ab44bd48a764401931a84582c38877d40b0b
yigal lipsey <yigalipsey1996@gmail.com>
2023-04-19 12:41:37 +0300

    ok

--- src/features/Task.jsx
+++ src/features/Task.jsx
@@ -1,17 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+let nextTaskId = 1
+
 export const todoListSlice = createSlice({
   name: 'todoList',
   initialState: {
-    tasks: [],
+    tasks: JSON.parse(localStorage.getItem('tasks')) || [],
   },
+
   reducers: {
     addTask: (state, action) => {
       state.tasks.push(action.payload)
+
+      localStorage.setItem('tasks', JSON.stringify(state.tasks))
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload)
+      localStorage.setItem('tasks', JSON.stringify(state.tasks))
+    },
+    completeTask: (state, action) => {
+      const task = state.tasks.find((task) => task.id === action.payload)
+      if (task) {
+        task.completed = true
+        localStorage.setItem('tasks', JSON.stringify(state.tasks))
+      }
+    },
+    updateTask: (state, action) => {
+      const task = state.tasks.find((task) => task.id === action.payload.id)
+      if (task) {
+        task.title = action.payload.title
+        localStorage.setItem('tasks', JSON.stringify(state.tasks))
+      }
     },
   },
 })
 
-export const { addTask } = todoListSlice.actions
+export const { addTask, deleteTask, completeTask, updateTask } =
+  todoListSlice.actions
 
 export default todoListSlice.reducer
